6eeb52a101c27bdfeadb173731288980e2f596b3
MacBook Pro <macbookpro@MacBook-Pro-van-MacBook.local>
2022-11-29 15:53:51 +0100

    added a path in urls.py for entry page, created entry function

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("error", views.error, name="error")
+    path("error", views.error, name="error"),
+    path("entry", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,11 +4,19 @@ from . import util
 
 
 def index(request):
+    """Returns a render of the index page"""
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def error(request):
+    """Returns a render of the error page"""
     return render(request, "encyclopedia/error.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, entry):
+    """Returns a render of the entry page"""
+    return render(request, "encyclopedia/entry.html", {
         "entries": util.list_entries()
     })
