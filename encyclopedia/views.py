65b0c06a058ec1f642cb0e88d69dea2856ca754b
MacBook Pro <macbookpro@MacBook-Pro-van-MacBook.local>
2022-11-29 16:29:16 +0100

    added a link for 'create a new link'

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,19 +4,25 @@ from . import util
 
 
 def index(request):
-    """Returns a render of the index page"""
+    """Returns a render of the index page."""
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def error(request):
-    """Returns a render of the error page"""
+    """Returns a render of the error page."""
     return render(request, "encyclopedia/error.html", {
-        "entries": util.list_entries()
+        "page": util.get_entry()
     })
 
 def entry(request, entry):
-    """Returns a render of the entry page"""
+    """Returns a render of the entry page."""
     return render(request, "encyclopedia/entry.html", {
         "entry": entry.capitalize()
     })
+
+def new_page(request):
+    """Returns a render of a page where the user
+    can create their own new page."""
+    return render(request, "encyclopedia/newpage.html", {
+        "page": util.get_entry()
