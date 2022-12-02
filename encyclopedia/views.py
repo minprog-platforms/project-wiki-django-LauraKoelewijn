866651759c1552a16826953faaaf81d2c4ccfa4b
MacBook Pro <macbookpro@MacBook-Pro-van-MacBook.local>
2022-12-02 14:03:48 +0100

    added markdown convert function + request in url works now, if page does not exist -> error page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 from . import util
+import markdown
 
+def convert_markdown(entry):
+    """Converts text from markdown to html and returns it
+    if the page is not empty"""
+    text = util.get_entry(entry)
+    while text is not None:
+        return True
+        return markdown.markdown(text)
+
+    return False
 
 def index(request):
     """Returns a render of the index page."""
@@ -12,14 +22,19 @@ def index(request):
 def error(request):
     """Returns a render of the error page."""
     return render(request, "encyclopedia/error.html", {
-        "page": util.list_entries()
+        "error": util.list_entries()
     })
 
 def entry(request, entry):
-    """Returns a render of the entry page."""
-    return render(request, "encyclopedia/entry.html", {
-        "entry": entry.capitalize()
-    })
+    """Returns a render of the entry page if the page is not empty. """
+    existing_page = convert_markdown(entry)
+    if existing_page == True:
+        return render(request, "encyclopedia/entry.html", {
+            "title": entry.capitalize(),
+            "content": existing_page
+            })
+    else:
+        return render(request, "encyclopedia/error.html")
 
 def new_page(request):
     """Returns a render of a page where the user
